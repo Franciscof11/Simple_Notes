d3b741f88f3e4c0318381072e49e187e7e80fd59
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-10-17 11:06:37 -0300

    feat: create note firestore

--- lib/controller/firestore_service.dart
+++ lib/controller/firestore_service.dart
@@ -1,1 +1,28 @@
-class FirestoreService {}
+// ignore_for_file: avoid_print
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final db = FirebaseFirestore.instance;
+
+class FirestoreService {
+  final note = {
+    'note': 'Bmw X6, Tracker Novo, Novo HRV',
+    'title': 'Carros pra comprar',
+    'timestamp': DateTime.now(),
+  };
+
+  // CREATE NOTE
+  Future<void> createNote() {
+    return db.collection('notes').add(note).then(
+          (note) {},
+        );
+  }
+
+  // DELETE NOTE
+  Future<void> deleteNote(String noteId) {
+    return db.collection('notes').doc(noteId).delete().then(
+          (doc) => print("Document deleted"),
+          onError: (e) => print("Error updating document $e"),
+        );
+  }
+}
