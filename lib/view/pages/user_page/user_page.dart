fa3f9d0827e2f541630d00ce56f320c9c3f7a027
Francisco Gabriel <franciscogabrielf11@gmail.com>
2023-10-27 19:11:33 -0300

    feat: upload photo from camera

--- lib/view/pages/user_page/user_page.dart
+++ lib/view/pages/user_page/user_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:simple_notes/controller/user/user_controller.dart';
 import 'package:simple_notes/view/pages/auth/sign_in_page.dart';
 
 import '../../../controller/auth/sign_out.dart';
@@ -13,6 +14,8 @@ class UserPage extends StatefulWidget {
 }
 
 class _UserPageState extends State<UserPage> {
+  final hasImage = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,13 +40,17 @@ class _UserPageState extends State<UserPage> {
             const SizedBox(height: 65),
             Stack(
               children: [
-                const SizedBox(
+                SizedBox(
                   width: 200,
                   height: 200,
                   child: CircleAvatar(
-                    foregroundImage: NetworkImage(
-                      'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png',
-                    ),
+                    foregroundImage: hasImage
+                        ? const NetworkImage(
+                            'https://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/1966.png',
+                          )
+                        : const NetworkImage(
+                            'https://cdn.icon-icons.com/icons2/2406/PNG/512/user_account_icon_145918.png',
+                          ),
                     backgroundColor: Colors.white,
                   ),
                 ),
@@ -51,7 +58,9 @@ class _UserPageState extends State<UserPage> {
                   bottom: 0,
                   right: 20,
                   child: GestureDetector(
-                    onTap: () => print('alo'),
+                    onTap: () {
+                      pickPhoto(context);
+                    },
                     child: Container(
                       padding: const EdgeInsets.all(7),
                       decoration: const BoxDecoration(
@@ -132,4 +141,88 @@ class _UserPageState extends State<UserPage> {
       ),
     );
   }
+}
+
+pickPhoto(context) {
+  showModalBottomSheet(
+    shape: const ContinuousRectangleBorder(
+        borderRadius: BorderRadius.only(
+      topRight: Radius.circular(70),
+      topLeft: Radius.circular(70),
+    )),
+    context: context,
+    builder: (context) => SizedBox(
+      height: MediaQuery.of(context).size.height * 0.25,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              const SizedBox(height: 10),
+              GestureDetector(
+                onTap: () {
+                  pegarImagemGaleria();
+                  Navigator.pop(context);
+                },
+                child: Row(
+                  children: [
+                    const Icon(Icons.photo_library_outlined, size: 30),
+                    const SizedBox(width: 12),
+                    Text(
+                      'Gallery',
+                      style: GoogleFonts.notoSans(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              GestureDetector(
+                onTap: () {
+                  pegarImagemCamera();
+                  Navigator.pop(context);
+                },
+                child: Row(
+                  children: [
+                    const Icon(Icons.camera_alt_outlined, size: 30),
+                    const SizedBox(width: 12),
+                    Text(
+                      'Camera',
+                      style: GoogleFonts.notoSans(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              GestureDetector(
+                onTap: () => print('removido'),
+                child: Row(
+                  children: [
+                    const Icon(Icons.delete_outline, size: 30),
+                    const SizedBox(width: 12),
+                    Text(
+                      'Remove',
+                      style: GoogleFonts.notoSans(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
 }
